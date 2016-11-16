e78ac5c2c209c2c29d649192cc05ddb5480aa9ff
yushengling <1121513947@qq.com>
2016-11-17 00:42:21 +0800

    添加一个toast

--- app/src/main/java/com/like/ling/MainActivity.java
+++ app/src/main/java/com/like/ling/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(getApplicationContext(), "nihao", Toast.LENGTH_SHORT).show();
+        //开始
     }
 }
