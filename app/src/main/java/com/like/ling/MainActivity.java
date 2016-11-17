9d9b4e1b248c77af6c55d2847962379f5508c044
wenlouis <si_yuan_wen@163.com>
2016-11-17 09:38:33 +0800

    添加了吐司哦

--- app/src/main/java/com/like/ling/MainActivity.java
+++ app/src/main/java/com/like/ling/MainActivity.java
@@ -13,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(getApplicationContext(), "nihao", Toast.LENGTH_SHORT).show();
         Toast.makeText(MainActivity.this, "yoyo", Toast.LENGTH_SHORT).show();
+
+        //我再来两个吐司好了
+        Toast.makeText(MainActivity.this, "吐司", Toast.LENGTH_SHORT).show();
     }
 }
